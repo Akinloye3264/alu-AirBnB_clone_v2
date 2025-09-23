6902c02f1b5b356496a9a18cb5cc0900c8bef6e3
Akinloye3264 <e.akinloye@alustudent.com>
2025-09-23 22:17:37 +0200

    changes of second  batch

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -130,3 +130,5 @@ class TestUser(unittest.TestCase):
         user = User()
         string = "[User] ({}) {}".format(user.id, user.__dict__)
         self.assertEqual(string, str(user))
+
+        
