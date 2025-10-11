0729e620449a8d24a00cc04b3ecab699211e0c84
Akinloye3264 <e.akinloye@alustudent.com>
2025-10-11 18:26:35 +0000

    pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module that starts a Flask web application"""
+"""
+Starts a Flask web application.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,21 +10,21 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Returns the string "Hello HBNB!" to the client"""
-    return 'Hello HBNB!'
+    """Return 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns the string "HBNB" to the client"""
-    return 'HBNB'
+    """Return 'HBNB'."""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def echo(text):
-    """Returns the string "C" followed by the value of the text variable"""
-    return "C {}".format(text.replace('_', ' '))
+def cisfun(text):
+    """Display “C ” followed by the value of the text variable."""
+    return "C " + text.replace("_", " ")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
