00ef034b81036ba041fbe390e3c144b08c86c731
Akinloye3264 <e.akinloye@alustudent.com>
2025-10-14 02:02:21 +0200

    changes again

--- models/state.py
+++ models/state.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-""" holds class State"""
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-import sqlalchemy
+from models import storage_t
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
+class State(BaseModel, Base if storage_t == "db" else object):
+    """ State class """
+    __tablename__ = 'states'
+    if storage_t == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
+            """Getter attribute cities that returns list of City instances"""
+            from models import storage
             city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
+            for city in storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
