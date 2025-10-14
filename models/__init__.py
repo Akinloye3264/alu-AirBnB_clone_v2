76ae2200d0d94bfa62dcc485827c1cb70bc006f9
Akinloye3264 <e.akinloye@alustudent.com>
2025-10-14 00:13:07 +0000

    omor

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
+"""Instantiates a storage object"""
 from os import getenv
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
+# Check which storage type should be used
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
 storage.reload()
